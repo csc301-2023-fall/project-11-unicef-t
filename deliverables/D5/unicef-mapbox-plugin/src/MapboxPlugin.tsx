e19920ba70cb56fc13b97198f1bf41fb75af979d
vala jalalvandi <vjalalvandi@gmail.com>
2023-12-08 01:26:31 -0500

    added comments to MapboxPlugin.tsx

--- deliverables/D5/unicef-mapbox-plugin/src/MapboxPlugin.tsx
+++ deliverables/D5/unicef-mapbox-plugin/src/MapboxPlugin.tsx
@@ -78,6 +78,7 @@ const MapboxPlugin = (props: MapboxPluginProps) => {
       zoom: 7 // starting zoom
     });
       
+    // Loading in default data (population density in rwanda)
     map.on('load', async() => {
       const response = await fetch(jsonFile);
       const geodata = await response.json();
@@ -147,6 +148,7 @@ const MapboxPlugin = (props: MapboxPluginProps) => {
   });
 
 
+  // Returning jsx with id "map", refering to the mapboxgl.Map instance above
   return (
     <div
     id="map"
